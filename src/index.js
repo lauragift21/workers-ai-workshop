466a9b8cb36d072fa38a2bf741d9527ccc98dc66
lauragift21 <laurandidi21@gmail.com>
2023-11-01 03:47:11 +0100

    first iteration

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
+import { Ai } from '@cloudflare/ai';
+
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+	async fetch(request, env) {
+		const ai = new Ai(env.AI)
+
+		const response = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+			prompt: 'Who was the first president of Nigeria?'
+		})
+
+		return new Response(JSON.stringify(response), {
+			headers: {
+				'content-type': 'application/json;charset=UTF-8',
+			},
+		})
+	}
+}
