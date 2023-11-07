3c497c672f41156793d06fc4ebe1deee1ecbae3a
lauragift21 <laurandidi21@gmail.com>
2023-11-07 16:10:14 +0100

    return default code

--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
-import { Ai } from '@cloudflare/ai';
-
 export default {
-	async fetch(request, env) {
-		const ai = new Ai(env.AI)
-
-		// personalize with chat style
-		const messages = [
-			{ role: 'system', content: 'You are an friendly assistant.' },
-			{ role: 'user', content: 'What is the weather like?' },
-		]
-
-		const stream = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
-			messages,
-			stream: true
-		});
-
-		return new Response(stream, {
-			headers: {
-				"content-type": "text/event-stream"
-			}
-		});
-	}
-}
+	async fetch(request, env, ctx) {
+		return new Response('Hello World!');
+	},
+};
