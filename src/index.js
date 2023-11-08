dbe87b14779533509f536a9dd8c7dfef1bbf0e98
lauragift21 <laurandidi21@gmail.com>
2023-11-08 10:24:26 +0100

    ui interface

--- src/index.js
+++ src/index.js
@@ -1,23 +1,34 @@
 import { Ai } from '@cloudflare/ai';
+import { html } from './template.js';
 
 export default {
 	async fetch(request, env) {
-		const ai = new Ai(env.AI)
 
-		// personalize with chat style
-		const messages = [
-			{ role: 'system', content: 'You are an friendly assistant.' },
-			{ role: 'user', content: 'What is the weather like?' },
-		]
+		const url = new URL(request.url);
+		if (request.method === 'POST' && url.pathname === '/ai') {
+			const ai = new Ai(env.AI);
 
-		const stream = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
-			messages,
-			stream: true
-		});
+			const body = await request.json();
+			const userMessage = body.message;
+			const messages = [
+				{ role: 'system', content: 'You are an friendly assistant.' },
+				{ role: 'user', content: userMessage },
+			]
 
-		return new Response(stream, {
+			const stream = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+				messages,
+				stream: true
+			});
+
+			return new Response(stream, {
+				headers: {
+					"content-type": "text/event-stream"
+				}
+			});
+		}
+		return new Response(html, {
 			headers: {
-				"content-type": "text/event-stream"
+				"content-type": "text/html;charset=UTF-8"
 			}
 		});
 	}
